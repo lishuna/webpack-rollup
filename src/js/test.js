40051ccd1057074ce4397daa472ca794ae553fa2
李淑娜 <lisna@jiedaibao.com>
2018-09-25 16:55:08 +0800

    update

---
+++ src/js/test.js
@@ -0,0 +1,4 @@
+import { square } from './math';
+
+let total = square(9);
+console.log('total: '+total);
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,36 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path');
+const ConsoleLogOnBuildWebpackPlugin = require('./ConsoleLogOnBuildWebpackPlugin');
 
 module.exports = {
-    entry: "./src/js/index.js",
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: "bundle.js"
-    },
-    mode: "development",
-    module: {
-        rules: [{
-            test: /\.html$/,
-            use: "html-loader"
-        }]
-    },
-    plugins: [
-        // new UglifyJsPlugin({
-        //     sourceMap: false,
-        //     uglifyOptions: {
-        //         compress: true,
-        //         warnings: true,
-        //         output: {
-        //             comments: true,
-        //             beautify: false
-        //         }
-        //     }
-        // })
-    ]
+  entry: {
+    main: './src/js/index.js',
+    test: './src/js/test.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: "[chunkhash:8].bundle.js",
+    publicPath: 'http://baidu.com/'
+  },
+  mode: "development",
+  module: {
+    rules: [{
+      test: /\.html$/,
+      use: "html-loader"
+    }]
+  },
+  plugins: [
+    new ConsoleLogOnBuildWebpackPlugin()
+    // new UglifyJsPlugin({
+    //     sourceMap: false,
+    //     uglifyOptions: {
+    //         compress: true,
+    //         warnings: true,
+    //         output: {
+    //             comments: true,
+    //             beautify: false
+    //         }
+    //     }
+    // })
+  ]
 };
